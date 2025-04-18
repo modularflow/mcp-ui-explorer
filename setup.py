274d74dbc99e4c958df4a75dd656719bf9740012
modularflow <rob@modularflow.ai>
2025-04-17 22:14:49 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README_MCP_UI_EXPLORER.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
